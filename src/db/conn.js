3724bbf6cae996e5c2059d1b82639373c29ebf01
Harsh2763 <97875850+Harsh2763@users.noreply.github.com>
2022-10-14 13:40:05 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,3 @@
 const mongoose = require('mongoose');
 const stockData = require('./model/stockSchema');
-const DB = 'mongodb+srv://harshoswal:Harsh%40123@cluster0.xy2s3p1.mongodb.net/stockinformation?retryWrites=true&w=majority';
-mongoose.connect(DB).then(()=>{
-    console.log('db connection successful');
-}).catch((e)=>{
-    console.log(e);
-});
+
